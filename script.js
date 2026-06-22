b97a76e3fee45cf3097ad596b4f9f5550ca9e708
ATA <kirvin114444@gmail.com>
2026-06-21 22:55:22 -0500

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const musica = document.getElementById("musica");
-
-document.addEventListener("click", () => {
-  musica.play().catch(() => {
-    console.log("El navegador bloqueó la música hasta que el usuario toque la pantalla.");
-  });
-}, { once: true });
